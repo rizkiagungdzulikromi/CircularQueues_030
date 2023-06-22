bb2bf19a36eddf0ce1a53a6efdb8a9021769835d
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-06-22 10:53:41 +0700

    dan menambahkan if dan else

--- Linklistqueue/Linklistqueue.cpp
+++ Linklistqueue/Linklistqueue.cpp
@@ -21,7 +21,7 @@ public:
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
-		}
+		} 
 
 		//cek apakah antrian kosong
 		if (FRONT == -1) {
