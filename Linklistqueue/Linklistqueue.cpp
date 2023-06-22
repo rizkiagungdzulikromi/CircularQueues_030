44a8b04614905f0386a64c7793abb5191a143ad0
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-06-22 11:06:55 +0700

    untuk mengecek apakah antrian kosong

--- Linklistqueue/Linklistqueue.cpp
+++ Linklistqueue/Linklistqueue.cpp
@@ -64,6 +64,12 @@ public:
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
 
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
 
+		cout << "\nElements in the queue are: \n";
 	}
 }; 
