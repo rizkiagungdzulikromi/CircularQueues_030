e7acaed38263b12419e9cc5a723e92a131afcd13
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-06-22 11:21:27 +0700

    //jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array

--- Linklistqueue/Linklistqueue.cpp
+++ Linklistqueue/Linklistqueue.cpp
@@ -74,11 +74,19 @@ public:
 
 		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
 		if (FRONT_position <= REAR_position) {
-			while (FRONT_position <= REAR_position)
-			cout << queue_array[FRONT_position] << "  ";
-			FRONT_position++;
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
-		cout << endl;
+		else {
+			//jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			
+			FRONT_position = 
 	}
-	else 
 }; 
