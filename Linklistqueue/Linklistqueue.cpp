8606a6db9363467a4e297c32db327586d6c9ff27
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-06-22 11:25:09 +0700

    interasi dari awal array hingga REAR

--- Linklistqueue/Linklistqueue.cpp
+++ Linklistqueue/Linklistqueue.cpp
@@ -86,7 +86,15 @@ public:
 				cout << queue_array[FRONT_position] << "  ";
 				FRONT_position++;
 			}
-			
-			FRONT_position = 
+
+			FRONT_position = 0;
+
+			//interasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
 	}
-}; 
+};
